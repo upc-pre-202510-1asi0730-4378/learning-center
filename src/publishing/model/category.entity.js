1fbd42fd753b7ccc1b9a2ffca593f712d8bd4f7b
Angel Velasquez <pcisavel@upc.edu.pe>
2025-05-03 12:04:30 -0500

    docs: add documentation to category entity.

--- src/publishing/model/category.entity.js
+++ src/publishing/model/category.entity.js
@@ -1,6 +1,26 @@
+/**
+ * @class Category
+ * @description Represents a category entity within the publishing system
+ * Used for categorizing content or organizing items
+ */
 export class Category {
+    /**
+     * Creates a new Category instance
+     * @param {Object} params - The category initialization parameters
+     * @param {string} [params.id=''] - Unique identifier for the category
+     * @param {string} [params.name=''] - Display name of the category
+     */
     constructor({id = '', name = ''}) {
+        /**
+         * @type {string}
+         * @description Unique identifier for the category
+         */
         this.id = id;
+        
+        /**
+         * @type {string}
+         * @description Display name of the category
+         */
         this.name = name;
     }
 }
