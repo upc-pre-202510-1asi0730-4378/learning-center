ae5c1543936673c48a7c7c3e00eb08d3c9f83545
Angel Velasquez <pcisavel@upc.edu.pe>
2025-05-03 15:22:09 -0500

    feat(about): add about us component.

---
+++ src/public/pages/about.component.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "about",
+  title: "About Us",
+}
+</script>
+
+<template>
+  <div class="w-full">
+    <h1>About Us</h1>
+    <p>ACME Learning Center is an Education Business Platform, part of ACME Corporation.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
