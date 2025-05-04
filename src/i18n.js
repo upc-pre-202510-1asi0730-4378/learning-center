bd4f4e7d4827ab2dc02d42785dc506944b541b7e
Angel Velasquez <pcisavel@upc.edu.pe>
2025-05-04 07:27:02 -0500

    feat(i18n): add language-dependent options.

--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,8 @@ import en from './locales/en.json';
 import es from './locales/es.json';
 import {createI18n} from "vue-i18n";
 
-const i18n = createI18n({locale: "en",
+const i18n = createI18n({
+    locale: "en",
     fallbackLocale: "en",
     globalInjection: true,
     messages: {en, es},});
